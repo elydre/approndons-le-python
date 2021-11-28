3b9c91752c89046ce09bc9f83acb7ff572bdddc8
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-11-28 11:20:33 +0100

    update

--- PyParser/sanalyseur.py
+++ PyParser/sanalyseur.py
@@ -31,18 +31,15 @@ def banana(string, model):
                 for fin in range(debut, len(string)+1):
                     if string[debut:fin] == m[0]:
                         start_pos = fin
-            for debut in range(start_pos,len(string)):
-                for fin in range(debut, len(string)+1):
-                    if string[debut:fin] == m[1]:
-                        end_pos = debut
-            if start_pos != None and end_pos != None:
-                return string[start_pos:end_pos]
-            else:
-                return None
-
-
-print(banana("for x in range(1,2,3):", "for |?| in range(|!|):"))
-print(banana("for x in range(1,2,3):", "for |!| in range(|?|):"))
+                        break
+            if start_pos != None:
+                for debut in range(start_pos,len(string)):
+                    for fin in range(debut, len(string)+1):
+                        if string[debut:fin] == m[1]:
+                            end_pos = debut
+                            break
+                if end_pos != None:
+                    return string[start_pos:end_pos]
 
 
 class line:
@@ -56,27 +53,16 @@ class line:
     def analyse(self):
         # parser python
         ligne = self.brut.split(" ")
+        ligne_brut = self.brut
         if ligne[0] == "for":
-            if "range" in ligne[3]:
-                args = [x.strip() for x in "".join(ligne[3:]).replace("range", "").strip()[1:-2].split(",")]
+            var = banana(ligne_brut, "for |?| in |!|")
+            if "range" in banana(ligne_brut, "for |!| in |?|"):
+                args = banana(ligne_brut, "for |!| in range(|?|):")
+                args = [arg.strip() for arg in args.split(",")]
                 if len(args) == 1:
-                    minimum = 0
-                    maximum = int(args[0])
-                    pas     = 1
+                    mini, maxi, pas = 0, args[0], 1
                 elif len(args) == 2:
-                    minimum = int(args[0])
-                    maximum = int(args[1])
-                    pas     = 1
+                    mini, maxi, pas = args[0], args[1], 1
                 elif len(args) == 3:
-                    minimum = int(args[0])
-                    maximum = int(args[1])
-                    pas     = int(args[2])
-                self.exit = {"type": "for-range",
-                            "var": ligne[1],
-                            "minimum": minimum,
-                            "maximum": maximum,
-                            "pas": pas}
-            else:
-                self.exit = {"type": "for-list",
-                            "var": ligne[1],
-                            "list": ligne[3].strip()[0:-1]}
+                    mini, maxi, pas = args[0], args[1], args[2]
+                self.exit = {"type": "for-range", "var": var, "min": mini, "max": maxi, "pas": pas}
--- test.py
+++ test.py
@@ -2,11 +2,9 @@ from PyParser.segmenteur import launch_segmenter as segmenter
 from PyParser.sanalyseur import line
 
 inp = """
+for x in range(20):
 for x in range(10,20):
-for x in test:
-if x + 3 == 1:
-elif x == 2:
-else:
+for x in range(10,20,2):
 """
 
 for i in inp.split("\n"):
