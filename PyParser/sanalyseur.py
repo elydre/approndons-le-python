22bd3635fd7a4c92e6b8644e6ba9a9251321b294
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-11-28 10:01:38 +0100

    Update sanalyseur.py

--- PyParser/sanalyseur.py
+++ PyParser/sanalyseur.py
@@ -1,5 +1,66 @@
 class line:
-    def __init__(self, inp: list):
+    def __init__(self, inp: str):
         self.brut = inp
         self.type = None
         self.value = None
+    
+    def analyse(self):
+        # parser python
+        match self.brut.split(" "):
+            case "for", var, "in range", end:
+                self.type = "for range"
+                self.value = 
+            case "if":
+                self.type = "if"
+                self.value = "if"
+            case "elif":
+                self.type = "elif"
+                self.value = "elif"
+            case "else":
+                self.type = "else"
+                self.value = "else"
+            case "while":
+                self.type = "while"
+                self.value = "while"
+            case "def":
+                self.type = "def"
+                self.value = "def"
+            case "class":
+                self.type = "class"
+                self.value = "class"
+            case "import":
+                self.type = "import"
+                self.value = "import"
+            case "from":
+                self.type = "from"
+                self.value = "from"
+            case "return":
+                self.type = "return"
+                self.value = "return"
+            case "print":
+                self.type = "print"
+                self.value = "print"
+            case "input":
+                self.type = "input"
+                self.value = "input"
+            case "break":
+                self.type = "break"
+                self.value = "break"
+            case "continue":
+                self.type = "continue"
+                self.value = "continue"
+            case "try":
+                self.type = "try"
+                self.value = "try"
+            case "except":
+                self.type = "except"
+                self.value = "except"
+            case "raise":
+                self.type = "raise"
+                self.value = "raise"
+            case "pass":
+                self.type = "pass"
+                self.value = "pass"
+            case "lambda":
+                self.type = "lambda"
+                self.value = "lambda"
