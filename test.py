1a1e69810e8b1653757bf5f0b3169cf421def750
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-12-04 19:20:02 +0100

    update

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ sortie = parser("""
 
 print("runing")
 for i in range(4000):
-    max = int(i ** 0.5 + 1) #2!
+    max = int(i ** 0.5 + 1)
     if i%2 != 0:
         for x in range(2, max):
             if i%x == 0:
